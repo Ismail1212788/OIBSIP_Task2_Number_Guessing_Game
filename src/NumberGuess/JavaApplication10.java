8451e7892b6c90df807e62626c35a1a8bbbc96fc
Muhammad Ismail <mi1936972@gmail.com>
2023-08-30 18:35:57 +0500

    initial commit

---
+++ src/NumberGuess/JavaApplication10.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package NumberGuess;
+
+/**
+ *
+ * @author kali-i
+ */
+
+public class JavaApplication10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        PlayForm a=new PlayForm();
+        a.show();
+        // TODO code application logic here
+    }
+    
+}
